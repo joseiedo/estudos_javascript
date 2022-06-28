00fa3e32c48937ffd85c32c12eb606d85edbd7b6
jose iedo <iedoduarte1@gmail.com>
2022-06-27 22:51:18 -0300

    update

--- objetos/strings/script.js
+++ objetos/strings/script.js
@@ -1,70 +1,76 @@
-// const comida = "Pizza";
-// const agua = new String("Agua");
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+const transacoes = [
+  {
+    descricao: "Taxa do Pão",
+    valor: "R$ 39",
+  },
+  {
+    descricao: "Taxa do Mercado",
+    valor: "R$ 129",
+  },
+  {
+    descricao: "Recebimento de Cliente",
+    valor: "R$ 99",
+  },
+  {
+    descricao: "Taxa do Banco",
+    valor: "R$ 129",
+  },
+  {
+    descricao: "Recebimento de Cliente",
+    valor: "R$ 49",
+  },
+];
 
-// console.log(agua.length);
-
-// const frase = "A melhor comida";
-
-// console.log(frase[frase.length - 1]);
-// console.log(frase.charAt(--frase.length));
-
-const frase = "A melhor linguagem é ";
-const linguagem = "JavaScript";
-
-const fraseFinal = frase.concat(linguagem, "!!", " Oi ahsaushs");
-
-const fruta = "Banana";
-const listaFrutas = "Melancia, Banana, Laranja";
-
-console.log(listaFrutas.includes(fruta, 14));
-
-console.log(fruta.endsWith("na"));
-
-const transacao1 = "Depósito de cliente";
-const transacao2 = "Depósito de fornecedor";
-const transacao3 = "Taxa de camisas";
-
-console.log(transacao1.slice(-5));
-
-console.log(fruta.indexOf("na"));
-
-const preco = "R$ 99,00";
-
-const listaPrecos = ["R$ 99", "R$ 199", "R$ 12000"];
-
-listaPrecos.forEach((item) => {
-  console.log(item.padStart(10, "-"));
+let taxaTotal = 0;
+let recebimentoTotal = 0;
+transacoes.forEach((item) => {
+  const valorItem = +item.valor.replace("R$ ", "");
+  if (item.descricao.slice(0, 4) === "Taxa") {
+    taxaTotal += valorItem;
+  } else if (item.descricao.startsWith("Recebimento")) {
+    recebimentoTotal += valorItem;
+  }
 });
 
-const frase2 = "Ta";
+console.log(taxaTotal);
+console.log(recebimentoTotal);
 
-frase2.repeat(5);
+// Retorne uma array com a lista abaixo
+const transportes = "Carro;Avião;Trem;Ônibus;Bicicleta";
+const transportesArray = transportes.split(";");
 
-let listaItens = "Camisas Bonés Calças Bermudas Vestidos Saias";
+// Substitua todos os span's por a's
+let html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+              </ul>`;
 
-listaItens = listaItens.replace(/[ ]+/g, ", ");
-console.log(listaItens);
+html = html.split("span").join("a");
+console.log(html);
 
-const arrayLista = listaItens.split("s, ");
-console.log(arrayLista);
+// Retorne o último caracter da frase
+const frase = "Melhor do ano!";
+const ultimoChar = frase.charAt(--frase.length);
+const ultimoChar2 = frase.slice(-1);
 
-const htmlText = "<div>O melhor item</div><div>A melhor lista</div>";
-const htmlArray = htmlText.split("div");
-const novoHtml = htmlArray.join("section");
+// Retorne o total de taxas
+const transacoes2 = [
+  "Taxa do Banco",
+  "   TAXA DO PÃO",
+  "  taxa do mercado",
+  "depósito Bancário",
+  "TARIFA especial",
+];
 
-console.log(htmlText);
-console.log(htmlArray);
-console.log(novoHtml);
+let contador = 0;
 
-const frutasArray = ["Banana", "Melancia", "Laranja"];
+transacoes2.forEach((item) => {
+  item = item.toLowerCase().trim();
 
-const sexo1 = "Feminino";
-const sexo2 = "feminino";
-const sexo3 = "FEMININO";
+  if (item.slice(0, 4) === "taxa") contador++;
+});
 
-console.log(sexo1.toUpperCase() === "FEMININO");
-
-const valor = "  R$ 23.00   ";
-valor.trim(); // 'R$ 23.00'
-valor.trimStart(); // 'R$ 23.00   '
-valor.trimEnd(); // '  R$ 23.00'
+console.log(`Há ${contador} taxas`);
