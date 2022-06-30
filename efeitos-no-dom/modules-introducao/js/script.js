76aaa75513c1c3f32b7296003d237898085a92ee
jose iedo <iedoduarte1@gmail.com>
2022-06-30 19:11:58 -0300

    efeitos

---
+++ efeitos-no-dom/modules-introducao/js/script.js
@@ -0,0 +1,16 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import * as teste from "./modules/teste.js";
+
+teste.teste2();
+teste.teste1();
+console.log(teste.senha);
+
+// console.log(this);
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
