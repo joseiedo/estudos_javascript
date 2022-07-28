bd1e0ebb9c81ee11d5eb08c9cf5d008319f6ec6c
jose iedo <iedoduarte1@gmail.com>
2022-07-28 19:35:12 -0300

    organizando arquivos

---
+++ mais-javascript/factory-function/script-aula.js
@@ -0,0 +1,29 @@
+// function createButton(text) {
+//   function element() {
+//     const buttonElement = document.createElement("button");
+//     buttonElement.innerText = text;
+//     return buttonElement;
+//   }
+//   return Object.freeze({
+//     text,
+//     element,
+//   });
+// }
+
+// const btnComprar = createButton("Comprar");
+// const btnVender = createButton("Vender");
+
+// console.log(btnComprar, btnVender);
+
+function Pessoa(nome) {
+  if (!(this instanceof Pessoa))
+    // ou (!new.target)
+    return new Pessoa(nome);
+  this.nome = nome;
+}
+
+Pessoa.prototype.andar = function () {
+  return `${this.nome} andou`;
+};
+
+const designer = Pessoa("André");
